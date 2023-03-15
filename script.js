01556d50bf7209159aeba1716075e576cbc16952
vanessarojas <96255792+vanessarojas@users.noreply.github.com>
2023-03-14 19:51:40 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-// Get the "Add to Cart" buttons and add event listeners to them
-var addToCartButtons = document.getElementsByClassName("add-to-cart");
-for (var i = 0; i < addToCartButtons.length; i++) {
-	addToCartButtons[i].addEventListener("click", addToCart);
-}
-
-// Get the cart link and add an event listener to it
-var cartLink = document.getElementById("cart-link");
-cartLink.addEventListener("click", showCart);
-
-// Initialize an empty shopping cart array
-var shoppingCart = [];
-
-// Define a function to add an item to the shopping cart
-function addToCart() {
-	var item = {
-		name: this.parentNode.getElementsByTagName("h3")[0].innerHTML,
-		price: parseFloat(this.parentNode.getElementsByTagName("p")[0].innerHTML.substr(8))
-	};
-	shoppingCart.push(item);
-	alert(item.name + " added to cart.");
-}
-
 // Initialize an empty shopping cart array
 var shoppingCart = [];
 
@@ -41,25 +18,14 @@ function addToCart() {
     price: parseFloat(this.parentNode.getElementsByTagName("p")[0].innerHTML.substr(1))
   };
   shoppingCart.push(item);
-  alert(item.name + " added to cart.");
+  alert(item.name + " has been added to your cart.");
 }
 
-// Define a function to checkout and display the contents of the shopping cart
+// Define a function to checkout and display the total price
 function checkout() {
-  var total = 0;
-  var cartContents = "Shopping Cart:\n";
+  var totalPrice = 0;
   for (var i = 0; i < shoppingCart.length; i++) {
-    cartContents += shoppingCart[i].name + " - $" + shoppingCart[i].price.toFixed(2) + "\n";
-    total += shoppingCart[i].price;
+    totalPrice += shoppingCart[i].price;
   }
-  cartContents += "Total: $" + total.toFixed(2);
-  alert(cartContents);
-  shoppingCart = [];
+  alert("Your total price is $" + totalPrice.toFixed(2));
 }
-
-
-// Define a function to display the contents of the shopping cart
-function showCart() {
-	var total = 0;
-	var cartContents = "Shopping Cart:\n";
-	for (
