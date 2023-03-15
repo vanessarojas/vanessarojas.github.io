93618c0bc0b47febad07abba57b5fb103f608b82
vanessarojas <96255792+vanessarojas@users.noreply.github.com>
2023-03-14 19:04:09 -0600

    Create home.php

---
+++ home.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Book Store</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<header>
+		<h1>Book Store</h1>
+		<nav>
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">Shop</a></li>
+				<li><a href="#">About</a></li>
+				<li><a href="#">Contact</a></li>
+			</ul>
+		</nav>
+	</header>
+	<main>
+		<section class="featured">
+			<h2>Featured Books</h2>
+			<div class="book">
+				<img src="book1.jpg" alt="Book 1">
+				<h3>Book 1</h3>
+				<p>Author: Author 1</p>
+				<p>Price: $10</p>
+				<button>Add to Cart</button>
+			</div>
+			<div class="book">
+				<img src="book2.jpg" alt="Book 2">
+				<h3>Book 2</h3>
+				<p>Author: Author 2</p>
+				<p>Price: $15</p>
+				<button>Add to Cart</button>
+			</div>
+			<div class="book">
+				<img src="book3.jpg" alt="Book 3">
+				<h3>Book 3</h3>
+				<p>Author: Author 3</p>
+				<p>Price: $20</p>
+				<button>Add to Cart</button>
+			</div>
+		</section>
+		<section class="new-arrivals">
+			<h2>New Arrivals</h2>
+			<div class="book">
+				<img src="book4.jpg" alt="Book 4">
+				<h3>Book 4</h3>
+				<p>Author: Author 4</p>
+				<p>Price: $25</p>
+				<button>Add to Cart</button>
+			</div>
+			<div class="book">
+				<img src="book5.jpg" alt="Book 5">
+				<h3>Book 5</h3>
+				<p>Author: Author 5</p>
+				<p>Price: $30</p>
+				<button>Add to Cart</button>
+			</div>
+			<div class="book">
+				<img src="book6.jpg" alt="Book 6">
+				<h3>Book 6</h3>
+				<p>Author: Author 6</p>
+				<p>Price: $35</p>
+				<button>Add to Cart</button>
+			</div>
+		</section>
+	</main>
+	<footer>
+		<p>&copy; 2023 Book Store</p>
+	</footer>
+</body>
+</html>
