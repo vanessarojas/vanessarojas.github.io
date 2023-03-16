bdd2bb4b1d8ccbba707f5b92d3ca27eec1aa7261
vanessarojas <96255792+vanessarojas@users.noreply.github.com>
2023-03-16 07:20:56 -0600

    Delete database.php

--- database.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = "localhost";
-$username = "username";
-$password = "password";
-$database_name = "bookstore";
-
-$database = new PDO("mysql:host=$host;dbname=$database_name", $username, $password);
-$database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
